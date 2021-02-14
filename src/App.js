19240d36b9ba3182c1b4f984a7e2ecb5581c13be
Andre Luiz Thiel <43017985+andrethiel@users.noreply.github.com>
2021-02-14 13:42:07 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header'
+import List from '@editorjs/editorjs'
+import ImageTool from '@editorjs/image';
+
+const editor = new EditorJS({
+  holderId : 'editorjs',
+  tools: { 
+    header: Header, 
+    list: List,
+    image: ImageTool
+  }, 
+  
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='editorjs' style={{margin: '1rem', border: 'solid 1px'}}>
+      
     </div>
   );
 }
---
+++ src/dist/bundle.js
@@ -0,0 +1,3 @@
+<script src='https://cdn.jsdelivr.net/npm/@editorjs/image@2.6.0/dist/bundle.min.js'>
+
+</script>
